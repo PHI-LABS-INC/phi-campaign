0a12aad1db1d91eabbaef4efc9cbd70d469c92e6
ZaK3939 <tsu.aria.kalei@gmail.com>
2023-01-30 15:04:18 +0400

    feat: update lens

--- 1-kakusei/index.ts
+++ 1-kakusei/index.ts
@@ -1,11 +1,14 @@
-import { ethers, Event } from "ethers";
-import { writeFile } from "node:fs/promises";
-import phiMapAbi from "./phiMapAbi";
-import { getENSFromLandURL, retry } from "./helper";
-import participants from "./participants.json" assert { type: "json" };
+import { ethers, Event } from 'ethers';
+import { writeFile } from 'node:fs/promises';
+import phiMapAbi from './phiMapAbi';
+import { getENSFromLandURL, retry } from './helper';
+import participants from './participants.json' assert { type: 'json' };
 
-const phiMapAddr = "0xe8b6395d223C9D3D85e162f2cb2023bC9088a908";
-const provider = new ethers.providers.AlchemyProvider("matic", process.env.POLYGON_ALCHEMY_API_KEY);
+const phiMapAddr = '0xe8b6395d223C9D3D85e162f2cb2023bC9088a908';
+const provider = new ethers.providers.AlchemyProvider(
+  'matic',
+  process.env.POLYGON_ALCHEMY_API_KEY
+);
 const contract = new ethers.Contract(phiMapAddr, phiMapAbi, provider);
 
 const main = async () => {
@@ -14,7 +17,17 @@ const main = async () => {
   const sizeLimit = 1000;
   const eventRequests: Promise<Event[]>[] = [];
   for (let i = start; i < end; i += sizeLimit) {
-    eventRequests.push(retry(() => contract.queryFilter(contract.filters.WriteLink(), i, i + sizeLimit > end ? end : i + sizeLimit), 3));
+    eventRequests.push(
+      retry(
+        () =>
+          contract.queryFilter(
+            contract.filters.WriteLink(),
+            i,
+            i + sizeLimit > end ? end : i + sizeLimit
+          ),
+        3
+      )
+    );
   }
   const events = (await Promise.all(eventRequests)).flat();
 
@@ -24,16 +37,24 @@ const main = async () => {
     if (event?.args) {
       const ens = getENSFromLandURL(event.args.url);
       if (ens) {
-        aggregateByENS[ens] = { ...aggregateByENS[ens], [event.args.name]: true };
+        aggregateByENS[ens] = {
+          ...aggregateByENS[ens],
+          [event.args.name]: true
+        };
       }
     }
   });
 
   // 3. sum and group them by address
-  const aggregateByAddress: { [address: string]: number } = participants.reduce((prev, addr) => ({ ...prev, [addr]: 0 }), {});
+  const aggregateByAddress: { [address: string]: number } = participants.reduce(
+    (prev, addr) => ({ ...prev, [addr]: 0 }),
+    {}
+  );
   await Promise.all(
     Object.keys(aggregateByENS).map(async (ens) => {
-      const addr = (await retry<string>(() => contract.ownerOfPhiland(ens.slice(0, -4)), 3)).toLowerCase();
+      const addr = (
+        await retry<string>(() => contract.ownerOfPhiland(ens.slice(0, -4)), 3)
+      ).toLowerCase();
       if (addr && aggregateByAddress[addr] !== undefined) {
         aggregateByAddress[addr] += Object.keys(aggregateByENS[ens]).length;
       }
@@ -46,8 +67,10 @@ const main = async () => {
   const ranking = Object.keys(aggregateByAddress)
     .map((address) => ({ address, score: aggregateByAddress[address] }))
     .sort((a, b) => b.score - a.score);
-  const csv = "address,score\n" + ranking.map((ranking) => `${ranking.address},${ranking.score}`).join("\n");
-  writeFile("./ranking.csv", csv);
+  const csv =
+    'address,score\n' +
+    ranking.map((ranking) => `${ranking.address},${ranking.score}`).join('\n');
+  writeFile('./ranking.csv', csv);
 };
 
 main();
---
+++ 2-yasai/index.ts
@@ -0,0 +1,164 @@
+import { ethers, Event } from 'ethers';
+import { writeFile } from 'node:fs/promises';
+import {
+  checkNFTBalance,
+  getCollectNFTAddress,
+  getDefaultProfile
+} from './lens';
+import participants from './participants.json';
+import { CountedData, Data, Score, UniqueRecord } from './type';
+
+require('dotenv').config();
+const provider = new ethers.providers.AlchemyProvider(
+  'matic',
+  process.env.POLYGON_ALCHEMY_API_KEY
+);
+
+const main = async () => {
+  const defaultProfile: Data = participants.reduce(
+    (prev, addr) => ({ ...prev, [addr]: '' }),
+    {}
+  );
+  // 1. Retrieve the defaultProfile of a participant in yasai.
+  await Promise.all(
+    Object.keys(defaultProfile).map(async (address) => {
+      const defaultProfileId = await getDefaultProfile(address);
+      if (defaultProfile) {
+        defaultProfile[address] = defaultProfileId;
+      }
+    })
+  ).catch((err) => {
+    throw err;
+  });
+
+  // 2. Exclude addresses with no set defaultProfile from the calculation.
+  const filteredParticipants: Data = Object.entries(defaultProfile).reduce(
+    (acc: Data, [key, value]) => {
+      if (!value.eq(0)) {
+        acc[key] = value;
+      }
+      return acc;
+    },
+    {}
+  );
+  console.log({ filteredParticipants });
+  // 3. fetch CollectNFTTransferred events during the Yasai event
+  const lensLPPAddr = '0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d';
+  const CollectNFTTransferredAbi = [
+    'event CollectNFTTransferred(uint256 indexed profileId, uint256 indexed pubId, uint256 indexed collectNFTId, address from, address to, uint256 timestamp)'
+  ];
+  const contract = new ethers.Contract(
+    lensLPPAddr,
+    CollectNFTTransferredAbi,
+    provider
+  );
+  const currentBlock = await provider.getBlockNumber();
+  const [start, end] = [37898365, currentBlock]; //Modify the start block after it has begun.
+  const sizeLimit = 1000;
+  const eventRequests: Promise<Event[]>[] = [];
+  for (let i = start; i < end; i += sizeLimit) {
+    eventRequests.push(
+      contract.queryFilter(
+        contract.filters.CollectNFTTransferred(),
+        i,
+        i + sizeLimit > end ? end : i + sizeLimit
+      )
+    );
+  }
+  const events = (await Promise.all(eventRequests)).flat();
+
+  // 4. Filter the event by requiring
+  // Both the collector and the collected person are campaign participants.
+  const filteredEvent = events.filter((item) => {
+    return (
+      item.args?.to &&
+      item.args?.profileId &&
+      item.args?.pubId &&
+      Object.keys(filteredParticipants).includes(item.args?.to.toLowerCase()) &&
+      Object.values(filteredParticipants).some((val) =>
+        val.eq(item.args?.profileId)
+      )
+    );
+  });
+  console.log(filteredEvent);
+  //5.Aggregate event where the three fields, to, profileId, and pubId are duplicated.
+  const uniqueEventData = Array.from(
+    new Set(
+      filteredEvent.map((item) => {
+        return {
+          collector: item.args?.to,
+          profileId: item.args?.profileId,
+          pubid: item.args?.pubId
+        };
+      })
+    )
+  );
+  console.log(uniqueEventData);
+  // 6.Verify if the address of the user (collector) who was the recipient has the collectNFT (profileId, pubId).
+  const balanceCheck = await Promise.all(
+    uniqueEventData.map(async (data) => {
+      const nftBalance = await checkNFTBalance(
+        data.collector,
+        await getCollectNFTAddress(data.profileId, data.pubid)
+      );
+      return nftBalance >= 1;
+    })
+  );
+  const filteredNFTData = uniqueEventData.filter((_, i) => balanceCheck[i]);
+  console.log(filteredNFTData);
+  // 7.Exclude self-collections.
+  const filteredData = filteredNFTData.filter((data) => {
+    return (
+      data.profileId.toHexString() !==
+      filteredParticipants[data.collector.toLowerCase()].toHexString()
+    );
+  });
+
+  //8.Prepare to measure the number of collections from unique addresses
+  const desiredData: UniqueRecord[] = filteredData.map((item) => ({
+    collector: item.collector,
+    profileId: item.profileId
+  }));
+  const uniqueData = [
+    ...new Set(
+      desiredData.map((item) =>
+        JSON.stringify({
+          collector: item.collector,
+          profileId: item.profileId.toHexString()
+        })
+      )
+    )
+  ].map((item) => JSON.parse(item));
+
+  // 9.Aggregated by profileId ex. { '0x010180': 2 }
+  const countedData: CountedData = uniqueData.reduce(
+    (result: CountedData, item: { profileId: string }) => {
+      if (!result[item.profileId]) {
+        result[item.profileId] = 0;
+      }
+      result[item.profileId] += 1;
+      return result;
+    },
+    {}
+  );
+  console.log(countedData);
+  // 10.Aggregated by address ex. { address: '0x5037e7747fAa78fc0ECF8DFC526DcD19f73076ce', score: 2 }
+  const lensScore: Score[] = Object.entries(filteredParticipants).map(
+    ([address, value]) => {
+      return { address, score: countedData[value.toHexString()] || 0 };
+    }
+  );
+  // 11. calc totalCollect
+  const totalCollect = lensScore.reduce((acc, item) => acc + item.score, 0);
+  console.log(totalCollect);
+  // 12. create sorted lensscore
+  lensScore.sort((a, b) => b.score - a.score);
+  const csv =
+    'address,score\n' +
+    lensScore
+      .map((ranking) => `${ranking.address},${ranking.score}`)
+      .join('\n');
+  writeFile('./ranking.csv', csv);
+};
+
+main();
